b9a45ee623bc44fa56b4dc9e27bb97b404a270d2
Anthony Diké <anthonydike@nyu.edu>
2021-09-08 16:34:26 -0400

    feat: export Inbox contract on compile

--- compile.js
+++ compile.js
@@ -6,7 +6,8 @@ const solc = require('solc'); // solidity compiler
 // allows for the file path to Inbox.sol to be understood cross-platform (Windows, Unix, etc)
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 // extracts the contents of the Inbox.sol file
-const source = fs.readFileSync(inboxPath, utf8);
+const source = fs.readFileSync(inboxPath, 'utf8');
 
 // sending the contents of the contract to the solidity compiler
-console.log(solc.compile(source, 1));
+// exports the contents to be read by other files that require/call this file
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // destructuring the Inbox object from inside the contracts object that's returned on compilation
