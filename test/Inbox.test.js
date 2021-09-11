f7b902919eca87b17ca452b8c61348a65b99936d
Anthony Diké <anthonydike@nyu.edu>
2021-09-11 16:08:03 -0400

    chore: removed Mocha test code from Inbox.test.js into Example.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,35 +4,3 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor of the web3.js library
 const web3 = new Web3(ganache.provider()); // instance
 
-// example test code
-
-class Car {
-  park() {
-    return 'stopped';
-  }
-
-  drive() {
-    return 'vroom';
-  }
-}
-
-// mocha test code
-
-// this code runs before each it() function
-let car;
-
-beforeEach(() => {
-  car = new Car();
-});
-
-describe('Car', () => {
-  it('can park', () => {
-    // create an instance of Car and test it
-    assert.equal(car.park(), 'stopped');
-  })
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  })
-})
-
