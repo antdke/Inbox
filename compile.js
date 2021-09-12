91b622141fa7c7f64a5c71050a3948a8f713e2c4
Anthony Diké <anthonydike@nyu.edu>
2021-09-12 19:04:19 -0400

    feat: created tests for whether the contract gets deployed and whether the message var in the contract has default value on deployment

--- compile.js
+++ compile.js
@@ -5,9 +5,10 @@ const solc = require('solc'); // solidity compiler
 
 // allows for the file path to Inbox.sol to be understood cross-platform (Windows, Unix, etc)
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-// extracts the contents of the Inbox.sol file
+// extracts the contents of the Inbox.sol file (the contract code)
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 // sending the contents of the contract to the solidity compiler
 // exports the contents to be read by other files that require/call this file
-module.exports = solc.compile(source, 1).contracts[':Inbox']; // destructuring the Inbox object from inside the contracts object that's returned on compilation
+module.exports = solc.compile(source, 1)['contracts'][':Inbox']; // destructuring the Inbox object from inside the contracts object that's returned on compilation
+
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,19 +3,31 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor of the web3.js library
 const web3 = new Web3(ganache.provider()); // instance
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
+const INITIAL_STRING = 'Hey Anthony! :)'
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
-
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(accounts);
+    // test if the contract has an address on the Eth network (aka it has been deployed)
+    assert.ok(inbox.options.address);
+  })
+
+  it('has a default message', async () => {
+    // test if the message var in the contract has a default value
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
   })
 })
